52c6187774c78dbfe7cc93fa2a3d2b7c19dc759f
Mohammad Fikri Nur Syahbani <72445001+mohammadfikrinursyahbani@users.noreply.github.com>
2023-03-25 21:50:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-# from sklearn.externals import joblib
+import joblib
 # Load the saved model
 model = joblib.load('xgb_model.pkl')
 
